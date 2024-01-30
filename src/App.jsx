9f9316ac126f57ec92fe2f7317b4721b293eb2e4
abhisheknagargoje <abhinagargoje63@gmail.com>
2024-01-31 00:48:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+function App() {
+	return (
+		<div className="">
+			<Button variant={"outline"}>Click me</Button>
+		</div>
+	);
+}
+
+export default App;
